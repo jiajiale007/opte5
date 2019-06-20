86dfe741e0f32a90d4547f7f67940ebd48520080
wjming <757130150@qq.com>
2019-06-20 23:46:57 +0800

    把本地工程添加到git

---
+++ fist-git-test/src/fitgGit/fistClass.java
@@ -0,0 +1,10 @@
+package fitgGit;
+
+public class fistClass {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++){
+            System.out.println("hello git project");
+        }
+
+    }
+}
